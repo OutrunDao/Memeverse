fe04d2e773ad67861698911d8c02d13f7154c413
Jason <2045330878@qq.com>
2024-06-17 20:25:50 +0800

    Add multicall

--- src/verse/Memeverse.sol
+++ src/verse/Memeverse.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Multicall.sol";
 
 import "./interfaces/IMemeverse.sol";
 import "../blast/GasManagerable.sol";
@@ -21,7 +22,7 @@ import "../token/FF.sol";
 import "../token/interfaces/IFF.sol";
 
 
-contract Memeverse is IMemeverse, Ownable, GasManagerable, Initializable, AutoIncrementId {
+contract Memeverse is IMemeverse, Multicall, Ownable, GasManagerable, Initializable, AutoIncrementId {
     using SafeERC20 for IERC20;
 
     address public constant USDB = 0x4200000000000000000000000000000000000022;
