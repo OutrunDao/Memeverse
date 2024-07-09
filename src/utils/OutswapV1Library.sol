7f9fffdbde1077f40d99b177f60e4fc612de7f2c
Jason <2045330878@qq.com>
2024-07-09 15:46:27 +0800

    update initcode

--- src/utils/OutswapV1Library.sol
+++ src/utils/OutswapV1Library.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.24;
 
-library OutswapV1Library {
+library OutswapV1Library02 {
     // returns sorted token addresses, used to handle return values from pairs sorted in this order
     function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
         require(tokenA != tokenB, "OutswapV1Library: IDENTICAL_ADDRESSES");
@@ -20,8 +20,8 @@ library OutswapV1Library {
                             hex"ff",
                             factory,
                             keccak256(abi.encodePacked(token0, token1)),
-                            /* bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(OutswapV1Pair).creationCode)); */
-                            hex"b11291cf74ef5b01d3360f4240ee3ae9b64c14297affa289ced01c032c687976" // init code hash
+                            /* bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(OutswapV1Pair).creationCode, abi.encode(gasManager))); */
+                            hex"d79ed2f68b5aeda6137657827786887c38c6283b2e9865d49f0f77e0f21d17bf" // 1% init code hash
                         )
                     )
                 )
