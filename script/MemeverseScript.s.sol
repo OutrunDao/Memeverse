be9ac9cf2427d892a2a2973e2ba0dd2ff66303f0
Jason <2045330878@qq.com>
2024-07-10 23:56:07 +0800

    deploy contract

--- script/MemeverseScript.s.sol
+++ script/MemeverseScript.s.sol
@@ -18,7 +18,7 @@ contract MemeverseScript is BaseScript {
         address orETHStakeManager = vm.envAddress("ORETH_STAKE_MANAGER");
         address orUSDStakeManager = vm.envAddress("ORUSD_STAKE_MANAGER");
         
-        address memeverse = address(new Memeverse(
+        Memeverse memeverse = new Memeverse(
             owner,
             gasManager,
             orETH,
@@ -29,10 +29,39 @@ contract MemeverseScript is BaseScript {
             orUSDStakeManager,
             factory,
             router
-        ));
-        console.log("Memeverse deployed on %s", memeverse);
+        );
+        address memeverseAddr = address(memeverse);
+        console.log("Memeverse deployed on %s", memeverseAddr);
 
-        address reserveFundManager = address(new ReserveFundManager(owner, gasManager, osETH, osUSD, memeverse));
+        address reserveFundManager = address(new ReserveFundManager(owner, gasManager, osETH, osUSD, memeverseAddr));
         console.log("ReserveFundManager deployed on %s", reserveFundManager);
+
+        uint256 genesisFee = 0.01 ether;
+        uint256 reserveFundRatio = 2000;
+        uint256 permanentLockRatio = 5000;
+        uint256 maxEarlyUnlockRatio = 70;
+        uint256 minEthFund = 8 ether;
+        uint256 minUsdbFund = 30000 ether;
+        uint128 minDurationDays = 1;
+        uint128 maxDurationDays = 7;
+        uint128 minLockupDays = 90;
+        uint128 maxLockupDays = 3650;
+        uint128 minfundBasedAmount = 1;
+        uint128 maxfundBasedAmount = 1000000;
+        memeverse.initialize(
+            reserveFundManager, 
+            genesisFee, 
+            reserveFundRatio, 
+            permanentLockRatio, 
+            maxEarlyUnlockRatio, 
+            minEthFund, 
+            minUsdbFund, 
+            minDurationDays, 
+            maxDurationDays, 
+            minLockupDays, 
+            maxLockupDays, 
+            minfundBasedAmount, 
+            maxfundBasedAmount
+        );
     }
 }
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,6 @@
 source ../.env
 forge clean && forge build
-forge script MemeverseScript.s.sol:FFLaunchScript --rpc-url blast_sepolia \
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url blast_sepolia \
     --priority-gas-price 300 --with-gas-price 1200000 \
-    --optimize --optimizer-runs 100000 \
+    --optimize --optimizer-runs 2000 \
     --broadcast --verify --ffi -vvvv
